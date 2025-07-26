b39c49548214656d9bf01891d5eea19c1e3765e8
hannah589 <hannahkxc@gmail.com>
2025-07-26 23:26:09 +1000

    Uploaded Article Preview Component Challenge
    Figured out how to change states based on breakpoint. When you click the button in mobile mode, it will show the mobile share content. And in desktop mode, an alternative pop-up will appear.
    Overcame position: absolute, blocking CSS styling, using a div within to handle the CSS styles.

--- article-preview-component/index.js
+++ article-preview-component/index.js
@@ -1,7 +1,26 @@
-const isMobileScreen = window.matchMedia("(max-width: 900px)").matches;
+function toggle(){
 
-if (isMobileScreen) {
-  // Your JavaScript code to display for mobile screens
-  console.log("This code runs on mobile-sized screens.");
-  // Example: document.getElementById("mobile-content").style.display = "block";
+  const content = document.getElementById("social-container");
+  const mobileContent = document.getElementById("mobile-share-content")
+  const isMobileScreen = window.matchMedia("(max-width: 900px)").matches;
+  const btnSection = document.getElementById("btn-section")
+  const profileCard = document.getElementById("profileCard")
+
+
+  if(content.style.display === "none" && !isMobileScreen){
+    content.style.display = "block";
+    btn.style.color = "hsl(217, 19%, 35%)"
+  } else{
+    content.style.display = "none";
+  }
+
+  if(isMobileScreen && mobileContent.style.display === "none") {
+    mobileContent.style.display = "block";
+    btnSection.style.backgroundColor = "hsl(217, 19%, 35%)"
+    profileCard.style.visibility = "hidden"
+  } else {
+    mobileContent.style.display = "none";
+    btnSection.style.backgroundColor = "white"
+    profileCard.style.visibility = "visible"
+  }
 }
